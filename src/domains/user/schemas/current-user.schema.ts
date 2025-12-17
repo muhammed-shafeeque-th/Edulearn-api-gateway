56a8dc31ffc559b15f79df7cfe1e6a7a4ec61526
shafeeque <shafeeqsha06@gmail.com>
2025-12-17 10:45:51 +0530

    feat(schema): add validation schema definitions for user rest endpoints

--- src/domains/user/schemas/current-user.schema.ts
+++ src/domains/user/schemas/current-user.schema.ts
@@ -1,7 +1,9 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const currentUserSchema = z.object({
-  userId: z.string({message: "userId be string type"}).min(1, { message: "userId is required" }),
+  userId: z
+    .string({ message: 'userId be string type' })
+    .min(1, { message: 'userId is required' }),
 });
 
 export type CurrentUserType = z.infer<typeof currentUserSchema>;
