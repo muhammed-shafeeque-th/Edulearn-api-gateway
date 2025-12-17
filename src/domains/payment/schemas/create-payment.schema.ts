0afa0f97cadffdfa14e7a704445a7cca45691838
shafeeque <shafeeqsha06@gmail.com>
2025-12-17 11:12:21 +0530

    fix(schema): fixed schemad defintion mismatches

--- src/domains/payment/schemas/create-payment.schema.ts
+++ src/domains/payment/schemas/create-payment.schema.ts
@@ -1,16 +1,30 @@
-import { PlaceOrderRequest } from '@/domains/service-clients/order/proto/generated/order_service';
-import { CreatePaymentRequest } from '@/domains/service-clients/payment/proto/generated/payment-service';
+import { Provider } from '@/domains/service-clients/payment/proto/generated/payment/common';
+import { CreatePaymentRequest } from '@/domains/service-clients/payment/proto/generated/payment_service';
 import { z, ZodType } from 'zod';
 
 export const createPaymentSchema: ZodType<CreatePaymentRequest> = z.object({
   orderId: z.string(),
   successUrl: z.string().optional(),
   cancelUrl: z.string().optional(),
-  amount: z.object({ amount: z.number(), currency: z.string() }),
-  paymentGateway: z.enum(['paypal', 'razorpay', 'stripe']),
+  provider: z
+    .enum(['paypal', 'razorpay', 'stripe'])
+    .transform((str): Provider => {
+      switch (str) {
+        case 'paypal':
+          return Provider.PAYPAL;
+        case 'razorpay':
+          return Provider.RAZORPAY;
+        case 'stripe':
+          return Provider.STRIPE;
+        default:
+          throw new Error(
+            'Invalid provider. Supported values are: paypal, razorpay, stripe'
+          );
+      }
+    }),
   /** For ensuring idempotency */
   idempotencyKey: z.string(),
   userId: z.string(),
-});
+}) as unknown as ZodType<CreatePaymentRequest>;
 
 export type CreatePaymentType = z.infer<typeof createPaymentSchema>;
--- src/domains/payment/schemas/verify-payment.schema.ts
+++ src/domains/payment/schemas/verify-payment.schema.ts
@@ -1,13 +1,30 @@
-import { PlaceOrderRequest } from '@/domains/service-clients/order/proto/generated/order_service';
-import {  RazorpayVerifyPaymentRequest } from '@/domains/service-clients/payment/proto/generated/payment-service';
 import { z, ZodType } from 'zod';
+import { verifyStripeSchema } from './verify-stripe.schema';
+import { verifyRazorpaySchema } from './verify-razorpay.schema';
+import { verifyPaypalSchema } from './verify-paypal.schema2';
+import { VerifyPaymentRequest } from '@/domains/service-clients/payment/proto/generated/payment_service';
+import { Provider } from '@/domains/service-clients/payment/proto/generated/payment/common';
 
+export const verifyPaymentSchema: ZodType<VerifyPaymentRequest> = z.object({
+  provider: z
+    .enum(['paypal', 'razorpay', 'stripe'])
+    .transform((str): Provider => {
+      switch (str) {
+        case 'paypal':
+          return Provider.PAYPAL;
+        case 'razorpay':
+          return Provider.RAZORPAY;
+        case 'stripe':
+          return Provider.STRIPE;
+        default:
+          throw new Error(
+            'Invalid provider. Supported values are: paypal, razorpay, stripe'
+          );
+      }
+    }),
+  stripe: verifyStripeSchema.optional(),
+  razorpay: verifyRazorpaySchema.optional(),
+  paypal: verifyPaypalSchema.optional(),
+}) as unknown as ZodType<VerifyPaymentRequest>;
 
-export const verifyRazorPaySchema: ZodType<RazorpayVerifyPaymentRequest> = z.object({
-  razorpayPaymentId: z.string(),
-  paymentId: z.string(),
-  razorpayOrderId: z.string(),
-  razorpaySignature: z.string(),
-});
-
-export type VerifyRazorPayType = z.infer<typeof verifyRazorPaySchema>;
+export type VerifyPaymentType = z.infer<typeof verifyPaymentSchema>;
