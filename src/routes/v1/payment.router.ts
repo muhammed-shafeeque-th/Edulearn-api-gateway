a4b8b51c6162a1e7408f19bc27972add70eb359d
shafeeque <shafeeqsha06@gmail.com>
2025-12-17 11:01:14 +0530

    refactor: removed payment router from v1 folder instead kept router version on domain router dir

--- src/routes/v1/payment.router.ts
+++
@@ -1,8 +0,0 @@
-import { blocklistMiddleware } from '../../middlewares/blocklist.middleware';
-import { paymentRoutesV1 } from '../../domains/payment/routers/v1/payment.router';
-import { Router } from 'express';
-
-const router = Router();
-router.use('/payments', paymentRoutesV1);
-
-export { router as paymentRoutesV1 };
