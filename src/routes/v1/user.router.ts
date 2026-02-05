f6d34b6d99d823647c56e2cc2a9ddaf5f1dae84c
shafeeque <shafeeqsha06@gmail.com>
2026-02-05 09:41:23 +0530

    refactor(router): refactored v1 router into a single router file with proper auth for protected endpoints

--- src/routes/v1/user.router.ts
+++
@@ -1,8 +0,0 @@
-import { blocklistMiddleware } from "../../middlewares/blocklist.middleware";
-import { userRoutesV1 } from "../../domains/user/routers/v1/users.router";
-import {Router} from "express";
-
-const router = Router();
-router.use("/users",  userRoutesV1);
-
-export {router as userRouterV1};
