df1cfeb8eca46d4e777b9275e0dca7060bc5450e
shafeeque <shafeeqsha06@gmail.com>
2026-02-05 07:43:02 +0530

    refactor(course): changed course validation schema dir from couser/validations/* to course/schemas/*

--- src/domains/course/validators/quiz/get-quiz-by-courseId.schema.ts
+++
@@ -1,9 +0,0 @@
-import {  GetQuizzesByCourseRequest } from '@/domains/service-clients/course/proto/generated/course_service';
-import { z, ZodType } from 'zod';
-
-
-export const getQuizzesByCourseSchema: ZodType<GetQuizzesByCourseRequest> = z.object({
-  courseId: z.string(),
-});
-
-export type GetQuizzesByCourseDto = z.infer<typeof getQuizzesByCourseSchema>;
