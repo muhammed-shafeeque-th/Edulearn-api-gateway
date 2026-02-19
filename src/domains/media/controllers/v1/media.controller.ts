a4fa5ddecc01c76f3da36e55bd90def94a391870
shafeeque <shafeeqsha06@gmail.com>
2026-02-19 23:00:30 +0530

    feat(media): migrate media controller, router  to use di container

--- src/domains/media/controllers/v1/media.controller.ts
+++ src/domains/media/controllers/v1/media.controller.ts
@@ -5,176 +5,120 @@ import { LoggingService } from 'services/observability/logging/logging.service';
 import { TracingService } from 'services/observability/tracing/trace.service';
 import { MetricsService } from '@/services/observability/metrics/metrics.service';
 import { HttpStatus } from '@/shared/constants/http-status';
-import { CloudinaryMediaService } from '@/services/media.service';
 import { avatarPresignedSchema } from '../../schemas/avatar.presigned.schema';
-import { s3StorageService, S3StorageService } from '@/services/storage.service';
 import { courseUploadPreSignSchema } from '../../schemas/course-presign.schema';
 import { getCoursePreSignSchema } from '../../schemas/get-course-presign.schema';
+import { CloudinaryMediaService } from '@/services/media/media.service';
+import {
+  s3StorageService,
+  S3StorageService,
+} from '@/services/media/storage.service';
+import { MEDIA_MESSAGES } from '../../utils/response-messages';
+import { inject, injectable } from 'inversify';
+import { Trace } from '@/shared/utils/decorators';
+import { TYPES } from '@/services/di';
 
-export class MediaService {
+@injectable()
+export class MediaController {
   private mediaService: CloudinaryMediaService;
   private storageService: S3StorageService;
-  private logger: LoggingService;
-  private tracer: TracingService;
-  private monitor: MetricsService;
 
-  constructor() {
-    // Observability services
-    this.logger = LoggingService.getInstance();
-    this.tracer = TracingService.getInstance();
-    this.monitor = MetricsService.getInstance();
-
+  constructor(@inject(TYPES.LoggingService) private logger: LoggingService) {
     // Business services
     this.mediaService = new CloudinaryMediaService();
     this.storageService = s3StorageService;
   }
-
+  @Trace('CourseController.generateAvatarUpdateSignature')
   async generateAvatarUpdateSignature(req: Request, res: Response) {
-    try {
-      await this.tracer.startActiveSpan(
-        'MediaService.generateAvatarUpdateSignature',
-        async span => {
-          this.logger.info(`Processing  method 'generateAvatarUpdateSignature'`);
+    this.logger.info(`Processing  method 'generateAvatarUpdateSignature'`);
 
-          const { uploadType, userId } = validateSchema(
-            { ...req.body, userId: req.user?.userId },
-            avatarPresignedSchema
-          )!;
-          span.setAttribute('request.userId', userId);
+    const { uploadType, userId } = validateSchema(
+      { ...req.body, userId: req.user?.userId },
+      avatarPresignedSchema
+    )!;
 
-          const response = this.mediaService.getSignedUploadUrl(
-            uploadType,
-            userId
-          );
+    const response = this.mediaService.getSignedUploadUrl(uploadType, userId);
 
-          this.logger.info('getSignedUploadUrl execution has completed ');
+    this.logger.info('getSignedUploadUrl execution has completed ');
 
-          return new ResponseWrapper(res)
-            .status(HttpStatus.OK)
-            .success(response, 'generated signed upload url successfully');
-        }
-      );
-    } catch (error: any) {
-      this.logger.error(
-        `Error calling  'getSignedUploadUrl' for user: ${req.user?.userId}: ${error.message}`
-      );
-
-      throw error;
-    }
+    return new ResponseWrapper(res)
+      .status(MEDIA_MESSAGES.GENERATED_AVATAR_UPLOAD_URL.statusCode)
+      .success(response, MEDIA_MESSAGES.GENERATED_AVATAR_UPLOAD_URL.message);
   }
-  async generateCourseUpdateUploadSignature(req: Request, res: Response) {
-    try {
-      await this.tracer.startActiveSpan(
-        'MediaService.generateCourseUpdateUploadSignature',
-        async span => {
-          this.logger.info(`Processing  method 'generateCourseUpdateUploadSignature'`);
 
-          const validPayload = validateSchema(
-            { ...req.body, userId: req.user?.userId },
-            courseUploadPreSignSchema
-          )!;
-          // span.setAttribute('request.userId', userId);
+  @Trace('CourseController.generateCourseUploadSignature')
+  async generateCourseUploadSignature(req: Request, res: Response) {
+    this.logger.info(`Processing  method 'generateCourseUploadSignature'`);
 
-          const response =
-            await this.storageService.generatePresignedUploadUrl(validPayload);
+    const validPayload = validateSchema(
+      { ...req.body, userId: req.user?.userId },
+      courseUploadPreSignSchema
+    )!;
 
-          this.logger.info(
-            'generateCourseUpdateUploadSignature execution has completed '
-          );
+    const response =
+      await this.storageService.generatePresignedUploadUrl(validPayload);
 
-          return new ResponseWrapper(res)
-            .status(HttpStatus.OK)
-            .success(response, 'generated signed upload url successfully');
-        }
-      );
-    } catch (error: any) {
-      this.logger.error(
-        `Error calling  'getSignedUploadUrl' for user: ${req.user?.userId}: ${error.message}`
-      );
+    this.logger.info('generateCourseUploadSignature execution has completed ');
 
-      throw error;
-    }
+    return new ResponseWrapper(res)
+      .status(MEDIA_MESSAGES.GENERATED_COURSE_UPLOAD_URL.statusCode)
+      .success(response, MEDIA_MESSAGES.GENERATED_COURSE_UPLOAD_URL.message);
   }
+
+  @Trace('CourseController.generateCourseUploadSecureSignature')
   async generateCourseUploadSecureSignature(req: Request, res: Response) {
-    try {
-      await this.tracer.startActiveSpan(
-        'MediaService.generateCourseUploadSecureSignature',
-        async span => {
-          this.logger.info(`Processing  method 'generateCourseUploadSecureSignature'`);
+    this.logger.info(
+      `Processing  method 'generateCourseUploadSecureSignature'`
+    );
 
-          const validPayload = validateSchema(
-            { ...req.body, userId: req.user?.userId },
-            courseUploadPreSignSchema
-          )!;
+    const validPayload = validateSchema(
+      { ...req.body, userId: req.user?.userId },
+      courseUploadPreSignSchema
+    )!;
 
-          const response =
-            await this.storageService.generateSecurePresignedUploadUrl(
-              validPayload
-            );
+    const response =
+      await this.storageService.generateSecurePresignedUploadUrl(validPayload);
 
-          this.logger.info(
-            'generateCourseUploadSecureSignature execution has completed '
-          );
+    this.logger.info(
+      'generateCourseUploadSecureSignature execution has completed '
+    );
 
-          return new ResponseWrapper(res)
-            .status(HttpStatus.OK)
-            .success(response, 'generated signed upload url successfully');
-        }
+    return new ResponseWrapper(res)
+      .status(MEDIA_MESSAGES.GENERATED_COURSE_UPLOAD_SECURE_URL.statusCode)
+      .success(
+        response,
+        MEDIA_MESSAGES.GENERATED_COURSE_UPLOAD_SECURE_URL.message
       );
-    } catch (error: any) {
-      this.logger.error(
-        `Error calling  'generateCourseUploadSecureSignature' for user: ${req.user?.userId}: ${error.message}`
-      );
-
-      throw error;
-    }
   }
 
+  @Trace('CourseController.generateSignedCourseUrl')
   async generateSignedCourseUrl(req: Request, res: Response) {
-    try {
-      await this.tracer.startActiveSpan(
-        'MediaService.generateSignedCourseUrl',
-        async span => {
-          this.logger.info(`Processing  method 'generateSignedCourseUrl'`);
+    this.logger.info(`Processing  method 'generateSignedCourseUrl'`);
 
-          const { key } = validateSchema(
-            { ...req.body, userId: req.user?.userId },
-            getCoursePreSignSchema
-          )!;
-          // span.setAttribute('request.userId', userId);
+    const { key } = validateSchema(
+      { ...req.body, userId: req.user?.userId },
+      getCoursePreSignSchema
+    )!;
 
-          const response =
-            await this.storageService.getSignedSecureCourseUrl(key);
+    const response = await this.storageService.getSignedSecureCourseUrl(key);
 
-          this.logger.info('generateSignedCourseUrl execution has completed ');
+    this.logger.info('generateSignedCourseUrl execution has completed ');
 
-          return new ResponseWrapper(res)
-            .status(HttpStatus.OK)
-            .success(response, 'generated signed upload url successfully');
-        }
-      );
-    } catch (error: any) {
-      this.logger.error(
-        `Error calling  'generateSignedCourseUrl' for user: ${req.user?.userId}: ${error.message}`
-      );
-
-      throw error;
-    }
+    return new ResponseWrapper(res)
+      .status(MEDIA_MESSAGES.GENERATED_SIGNED_COURSE_URL.statusCode)
+      .success(response, MEDIA_MESSAGES.GENERATED_SIGNED_COURSE_URL.message);
   }
+
+  @Trace('CourseController.multipartSignInit')
   async multipartSignInit(req: Request, res: Response) {
-    try {
-      await this.tracer.startActiveSpan(
-        'MediaService.getSignedUploadUrl',
-        async span => {
-          this.logger.info(`Processing  method 'getSignedUploadUrl'`);
+    this.logger.info(`Processing  method 'getSignedUploadUrl'`);
 
-          const { uploadType, userId } = validateSchema(
-            { ...req.body, userId: req.user?.userId },
-            avatarPresignedSchema
-          )!;
-          span.setAttribute('request.userId', userId);
+    const { uploadType, userId } = validateSchema(
+      { ...req.body, userId: req.user?.userId },
+      avatarPresignedSchema
+    )!;
 
-          /**
+    /**
            * return this.storage.createMultipartUpload({
     filename: dto.filename,
     contentType: dto.contentType,
@@ -184,135 +128,78 @@ export class MediaService {
   });
            */
 
-          const response = this.mediaService.getSignedUploadUrl(
-            uploadType,
-            userId
-          );
+    const response = this.mediaService.getSignedUploadUrl(uploadType, userId);
 
-          this.logger.info('getSignedUploadUrl execution has completed ');
+    this.logger.info('getSignedUploadUrl execution has completed ');
 
-          return new ResponseWrapper(res)
-            .status(HttpStatus.OK)
-            .success(response, 'generated signed upload url successfully');
-        }
-      );
-    } catch (error: any) {
-      this.logger.error(
-        `Error calling  'getSignedUploadUrl' for user: ${req.user?.userId}: ${error.message}`
-      );
-
-      throw error;
-    }
+    return new ResponseWrapper(res)
+      .status(MEDIA_MESSAGES.MULTIPART_SIGN_INIT.statusCode)
+      .success(response, MEDIA_MESSAGES.MULTIPART_SIGN_INIT.message);
   }
+
+  @Trace('CourseController.multipartSignComplete')
   async multipartSignComplete(req: Request, res: Response) {
-    try {
-      await this.tracer.startActiveSpan(
-        'MediaService.getSignedUploadUrl',
-        async span => {
-          this.logger.info(`Processing  method 'getSignedUploadUrl'`);
+    this.logger.info(`Processing  method 'getSignedUploadUrl'`);
 
-          const { uploadType, userId } = validateSchema(
-            { ...req.body, userId: req.user?.userId },
-            avatarPresignedSchema
-          )!;
-          span.setAttribute('request.userId', userId);
+    const { uploadType, userId } = validateSchema(
+      { ...req.body, userId: req.user?.userId },
+      avatarPresignedSchema
+    )!;
 
-          /**
+    /**
            return this.storage.completeMultipartUpload(dto.key, dto.uploadId, dto.parts);
            */
 
-          const response = this.mediaService.getSignedUploadUrl(
-            uploadType,
-            userId
-          );
+    const response = this.mediaService.getSignedUploadUrl(uploadType, userId);
 
-          this.logger.info('getSignedUploadUrl execution has completed ');
+    this.logger.info('getSignedUploadUrl execution has completed ');
 
-          return new ResponseWrapper(res)
-            .status(HttpStatus.OK)
-            .success(response, 'generated signed upload url successfully');
-        }
-      );
-    } catch (error: any) {
-      this.logger.error(
-        `Error calling  'getSignedUploadUrl' for user: ${req.user?.userId}: ${error.message}`
-      );
-
-      throw error;
-    }
+    return new ResponseWrapper(res)
+      .status(MEDIA_MESSAGES.MULTIPART_SIGN_COMPLETE.statusCode)
+      .success(response, MEDIA_MESSAGES.MULTIPART_SIGN_COMPLETE.message);
   }
+
+  @Trace('CourseController.multipartSignGetParts')
   async multipartSignGetParts(req: Request, res: Response) {
-    try {
-      await this.tracer.startActiveSpan(
-        'MediaService.getSignedUploadUrl',
-        async span => {
-          this.logger.info(`Processing  method 'getSignedUploadUrl'`);
+    this.logger.info(`Processing  method 'getSignedUploadUrl'`);
 
-          const { uploadType, userId } = validateSchema(
-            { ...req.body, userId: req.user?.userId },
-            avatarPresignedSchema
-          )!;
-          span.setAttribute('request.userId', userId);
+    const { uploadType, userId } = validateSchema(
+      { ...req.body, userId: req.user?.userId },
+      avatarPresignedSchema
+    )!;
 
-          /**
+    /**
             return this.storage.getMultipartPartUrls(dto.key, dto.uploadId, dto.partNumbers);
            */
 
-          const response = this.mediaService.getSignedUploadUrl(
-            uploadType,
-            userId
-          );
+    const response = this.mediaService.getSignedUploadUrl(uploadType, userId);
 
-          this.logger.info('getSignedUploadUrl execution has completed ');
+    this.logger.info('getSignedUploadUrl execution has completed ');
 
-          return new ResponseWrapper(res)
-            .status(HttpStatus.OK)
-            .success(response, 'generated signed upload url successfully');
-        }
-      );
-    } catch (error: any) {
-      this.logger.error(
-        `Error calling  'getSignedUploadUrl' for user: ${req.user?.userId}: ${error.message}`
-      );
-
-      throw error;
-    }
+    return new ResponseWrapper(res)
+      .status(MEDIA_MESSAGES.MULTIPART_SIGN_PARTS.statusCode)
+      .success(response, MEDIA_MESSAGES.MULTIPART_SIGN_PARTS.message);
   }
+
+  @Trace('CourseController.multipartSignAbort')
   async multipartSignAbort(req: Request, res: Response) {
-    try {
-      await this.tracer.startActiveSpan(
-        'MediaService.getSignedUploadUrl',
-        async span => {
-          this.logger.info(`Processing  method 'getSignedUploadUrl'`);
+    this.logger.info(`Processing  method 'getSignedUploadUrl'`);
 
-          const { uploadType, userId } = validateSchema(
-            { ...req.body, userId: req.user?.userId },
-            avatarPresignedSchema
-          )!;
-          span.setAttribute('request.userId', userId);
+    const { uploadType, userId } = validateSchema(
+      { ...req.body, userId: req.user?.userId },
+      avatarPresignedSchema
+    )!;
 
-          /**
+    /**
              return this.storage.abortMultipartUpload(body.key, body.uploadId);
            */
 
-          const response = this.mediaService.getSignedUploadUrl(
-            uploadType,
-            userId
-          );
+    const response = this.mediaService.getSignedUploadUrl(uploadType, userId);
 
-          this.logger.info('getSignedUploadUrl execution has completed ');
+    this.logger.info('getSignedUploadUrl execution has completed ');
 
-          return new ResponseWrapper(res)
-            .status(HttpStatus.OK)
-            .success(response, 'generated signed upload url successfully');
-        }
-      );
-    } catch (error: any) {
-      this.logger.error(
-        `Error calling  'getSignedUploadUrl' for user: ${req.user?.userId}: ${error.message}`
-      );
-
-      throw error;
-    }
+    return new ResponseWrapper(res)
+      .status(MEDIA_MESSAGES.MULTIPART_SIGN_ABORT.statusCode)
+      .success(response, MEDIA_MESSAGES.MULTIPART_SIGN_ABORT.message);
   }
 }
