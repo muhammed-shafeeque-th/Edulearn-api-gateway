bb3785182fc27234ad345941b931a0be46a26e16
shafeeque <shafeeqsha06@gmail.com>
2026-03-06 21:57:18 +0530

    feat(discussion): add discussion response mapper entity

---
+++ src/domains/chat/utils/discussion.mappers.ts
@@ -0,0 +1,36 @@
+import {
+  DiscussionMessageData,
+  DiscussionRoomData,
+  MessageData,
+} from '@/domains/service-clients/chat/proto/generated/chat_service';
+import { Discussion, DiscussionMessage, Message } from '../types';
+import { UserInfo } from '@/domains/user/types';
+
+export class DiscussionResponseMapper {
+  public static toDiscussion = (chat: DiscussionRoomData): Discussion => {
+    return {
+      id: chat.id,
+      instructorId: chat.instructorId,
+      createdAt: chat.createdAt,
+      updatedAt: chat.updatedAt,
+      courseId: chat.courseId,
+    };
+  };
+
+  public static toDiscussionMessage = (
+    message: DiscussionMessageData,
+    sender?: UserInfo
+  ): DiscussionMessage => {
+    return {
+      id: message.id,
+      roomId: message.roomId,
+      senderRole: message.senderRole,
+      sender: sender,
+      senderId: message.senderId,
+      content: message.content,
+      createdAt: message.createdAt,
+      updatedAt: message.updatedAt,
+      sequence: message.sequence,
+    };
+  };
+}
