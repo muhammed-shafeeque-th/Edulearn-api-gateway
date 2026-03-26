52268a23f597a46940ce4502712332a18ec380e7
shafeeque <shafeeqsha06@gmail.com>
2026-03-26 23:09:10 +0530

    refactor: refactored course scheams into course/v1/schemas* dir

--- src/domains/course/schemas/course/get-course.schema.ts
+++
@@ -1,8 +0,0 @@
-import { GetCourseRequest } from '@/domains/service-clients/course/proto/generated/course/types/course';
-import { z, ZodType } from 'zod';
-
-export const getCourseSchema: ZodType<GetCourseRequest> = z.object({
-  courseId: z.string().uuid(),
-});
-
-export type GetCourseDto = z.infer<typeof getCourseSchema>;
