0c66b4df2bd9e7eb957b182a10f801d2004c572d
shafeeque <shafeeqsha06@gmail.com>
2026-04-06 10:08:32 +0530

    refactor(payment): refactor payment router, controller and utilities to src/domains/payment/v1*

--- src/domains/payment/v1/types/index.ts
+++ src/domains/payment/v1/types/index.ts
@@ -1,37 +1,3 @@
-
-export interface Course {
-  id: string;
-  title?: string | undefined;
-  topics: string[];
-  instructorId?: string | undefined;
-  subTitle?: string | undefined;
-  category?: string | undefined;
-  subCategory?: string | undefined;
-  language?: string | undefined;
-  subtitleLanguage?: string | undefined;
-  level?: string | undefined;
-  durationValue?: string | undefined;
-  durationUnit?: string | undefined;
-  description?: string | undefined;
-  learningOutcomes: string[];
-  targetAudience: string[];
-  requirements: string[];
-  thumbnail?: string | undefined;
-  trailer?: string | undefined;
-  status?: string | undefined;
-  slug: string | undefined;
-  rating: number | undefined;
-  totalRatings: number | undefined;
-  numberOfRating?: number | undefined;
-  enrollments?: number | undefined;
-  createdAt: string;
-  updatedAt: string;
-  deletedAt?: string | undefined;
-  price: number | undefined;
-  discountPrice?: number | undefined;
-  currency?: string | undefined;
-}
-
 export type PaymentProvider = 'paypal' | 'razorpay' | 'stripe';
 
 export interface OrderItems {
