d5d20a68142f8bf319c1f3772fa29e26c3afb7f6
shafeeque <shafeeqsha06@gmail.com>
2026-05-20 23:08:06 +0530

    feat(route): add route constant to avoid magic string in route

---
+++ src/domains/auth/v1/routers/route.constants.ts
@@ -0,0 +1,14 @@
+export const authEndpoints = {
+  csrfToken: '/csrf-token',
+  register: '/register',
+  oauth: '/oauth',
+  emailCheck: '/email-check',
+  login: '/login',
+  logout: '/logout',
+  refresh: '/refresh',
+  verify: '/verify',
+  resendOtp: '/resend-otp',
+  resetPassword: '/reset-password',
+  changePassword: '/change-password',
+  forgotPassword: '/forgot-password',
+};
