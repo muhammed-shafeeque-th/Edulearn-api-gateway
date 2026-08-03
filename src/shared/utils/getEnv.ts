305f630ae1f1e05bd82cbba323db438b2cf23a33
shafeeque <shafeeqsha06@gmail.com>
2026-08-03 14:07:59 +0530

    feat(utils): update application utitlity functions

--- src/shared/utils/getEnv.ts
+++ src/shared/utils/getEnv.ts
@@ -1,18 +1,1 @@
-import dotenv from 'dotenv';
-import { EnvNotFoundError } from '../errors/env-not-found-error';
-dotenv.config();
-
-type EnvironmentVariables = {
-  [key: string]: string;
-};
-
-export function getEnvs(...envs: string[]): EnvironmentVariables {
-  const variables: EnvironmentVariables = {};
-  envs.forEach(env => {
-    if (!process.env[env]) {
-      throw new EnvNotFoundError(env);
-    }
-    variables[env] = process.env[env];
-  });
-  return variables;
-}
+export { getEnvs } from '@edulearn/core';
