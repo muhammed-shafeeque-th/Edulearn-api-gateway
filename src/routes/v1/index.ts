5431fe517e33b670897734536589ea4627c0ae6c
shafeeque <shafeeqsha06@gmail.com>
2026-08-03 14:19:38 +0530

    feat(v1): update application v1 routes, add route path configs to formalize route paths

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,50 +1,33 @@
-import { adminRoutesV1 } from '@/domains/admin/routers/v1/admin.router';
-import { authRoutesV1 } from '@/domains/auth/routers/v1/auth.router';
-import { cartRoutesV1 } from '@/domains/cart/routers/v1/cart.router';
-import { chatRoutesV1 } from '@/domains/chat/routers/v1/chat.router';
-import { discussionRoutesV1 } from '@/domains/chat/routers/v1/discussion.router';
-import { courseRoutesV1 } from '@/domains/course/routers/v1/course.router';
-import { enrollmentRoutesV1 } from '@/domains/enrollment/routers/v1/enrollment.router';
-import { mediaRoutesV1 } from '@/domains/media/routers/v1/media.router';
-import { notificationRoutesV1 } from '@/domains/notification/routers/v1/notification.router';
-import { orderRoutesV1 } from '@/domains/order/routers/v1/order.router';
-import { paymentRoutesV1 } from '@/domains/payment/routers/v1/payment.router';
-import { userRoutesV1 } from '@/domains/user/routers/v1/users.router';
-import { walletRoutesV1 } from '@/domains/wallet/routers/v1/wallet.router';
-import { wishlistRoutesV1 } from '@/domains/wishlist/routers/v1/wishlist.router';
-import { authenticate } from '@/middlewares/auth.middleware';
+import { adminRoutesV1 } from '@/domains/admin/v1/routers';
+import { authRoutesV1 } from '@/domains/auth/v1/routers';
+import { chatRoutesV1 } from '@/domains/chat/v1/routers/chat.router';
+import { discussionRoutesV1 } from '@/domains/chat/v1/routers/discussion.router';
+import { courseRouterV1 } from '@/domains/course/v1/routers/';
+import { enrollmentRouterV1 } from '@/domains/enrollment/v1/routers';
+import { mediaRouterV1 } from '@/domains/media/v1/routers';
+import { notificationRouterV1 } from '@/domains/notification/v1/routers';
+import { orderRoutesV1 } from '@/domains/order/v1/routers';
+import { paymentRoutesV1 } from '@/domains/payment/v1/routers';
+import { userRouterV1 } from '@/domains/user/v1/routers';
+import { instructorRouterV1 } from '@/domains/user/v1/routers/instructor.router';
+import { authGuard } from '@/middlewares/auth.middleware';
 import { blocklistMiddleware } from '@/middlewares/blocklist.middleware';
+import { adminRateLimiter } from '@/services/security/ratelimiter';
 import { Router } from 'express';
+import { BaseRoutes } from './route.constants';
 
 const router = Router();
-router.use('/users', userRoutesV1);
-router.use('/auth', authRoutesV1);
-router.use('/media', authenticate, blocklistMiddleware, mediaRoutesV1);
-router.use('/courses', courseRoutesV1);
-router.use('/carts', authenticate, blocklistMiddleware, cartRoutesV1);
-router.use('/chats', authenticate, blocklistMiddleware, chatRoutesV1);
-router.use(
-  '/discussions',
-  authenticate,
-  blocklistMiddleware,
-  discussionRoutesV1
-);
-router.use('/wishlists', authenticate, blocklistMiddleware, wishlistRoutesV1);
-router.use('/orders', authenticate, blocklistMiddleware, orderRoutesV1);
-router.use('/payments', authenticate, blocklistMiddleware, paymentRoutesV1);
-router.use(
-  '/enrollments',
-  authenticate,
-  blocklistMiddleware,
-  enrollmentRoutesV1
-);
-router.use('/wallets', authenticate, blocklistMiddleware, walletRoutesV1);
-router.use(
-  '/notifications',
-  authenticate,
-  blocklistMiddleware,
-  notificationRoutesV1
-);
-router.use('/admin', adminRoutesV1);
+router.use(BaseRoutes.users, userRouterV1);
+router.use(BaseRoutes.instructors, instructorRouterV1);
+router.use(BaseRoutes.auth, authRoutesV1);
+router.use(BaseRoutes.media, authGuard(), mediaRouterV1);
+router.use(BaseRoutes.courses, courseRouterV1);
+router.use(BaseRoutes.chats, authGuard(), chatRoutesV1);
+router.use(BaseRoutes.discussions, authGuard(), discussionRoutesV1);
+router.use(BaseRoutes.orders, authGuard(), orderRoutesV1);
+router.use(BaseRoutes.payments, authGuard(), paymentRoutesV1);
+router.use(BaseRoutes.enrollments, authGuard(), enrollmentRouterV1);
+router.use(BaseRoutes.notifications, authGuard(), notificationRouterV1);
+router.use(BaseRoutes.admin, adminRoutesV1);
 
 export { router as routerV1 };
---
+++ src/routes/v1/route.constants.ts
@@ -0,0 +1,14 @@
+export const BaseRoutes = {
+  users: '/users',
+  instructors: '/instructors',
+  auth: '/auth',
+  media: '/media',
+  courses: '/courses',
+  chats: '/chats',
+  discussions: '/discussions',
+  orders: '/orders',
+  payments: '/payments',
+  enrollments: '/enrollments',
+  notifications: '/notifications',
+  admin: '/admin',
+};
